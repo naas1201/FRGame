bd015f6b8eb3844ccf013897ecd33ab756666993
Q <88073861+naas1201@users.noreply.github.com>
2025-05-31 10:12:21 -0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/votrenom/francerraison/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.votrenom.francerraison // Adaptez à votre package
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Nous lierons le layout et ajouterons la logique du jeu ici
+        // setContentView(R.layout.activity_main) // Sera activé bientôt
+    }
+}
