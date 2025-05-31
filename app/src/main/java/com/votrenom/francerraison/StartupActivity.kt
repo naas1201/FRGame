060c453aed7f7b7a42fdffd919430e660ddb00b4
Q <88073861+naas1201@users.noreply.github.com>
2025-05-31 09:54:35 -0700

    Update StartupActivity.kt

--- app/src/main/java/com/votrenom/francerraison/StartupActivity.kt
+++ app/src/main/java/com/votrenom/francerraison/StartupActivity.kt
@@ -1,9 +1,11 @@
-<?xml version="1.0" encoding="utf-8"?>
-<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:app="http://schemas.android.com/apk/res-auto"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context=".StartupActivity">
+package com.votrenom.francerraison // Adaptez à votre package
 
-    </androidx.constraintlayout.widget.ConstraintLayout>
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StartupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Nous ajouterons la liaison au layout (setContentView) et la logique ici bientôt.
+    }
+}
